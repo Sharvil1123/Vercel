35d5c131b549024db7728a5005e535f9490366bd
sharvil <sharvilbhalke@gmail.com>
2024-02-11 19:28:06 +0530

    initializd endpoint for repourl

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,14 @@ import cors from "cors";
 const app = express();
 app.use(cors())
 
+// initialized an endpoint that the usr will hit and send the repo url as input
+app.use(express.json())
+app.post("/delpoy",(req, res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+
+    res.json({})
+})
+
 app.listen(3000);
+
