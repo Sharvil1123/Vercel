d949f88c316910c41031000b577c57e770c5ba37
sharvil <sharvilbhalke@gmail.com>
2024-02-11 19:36:09 +0530

    id generation function crated

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+// creatd a gnerat function for id generation.
+const MAX_LEN = 5; 
+
+export function generate(){
+    let ans = "";
+    const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
+    for(let i = 0; i < MAX_LEN ; i++){
+        ans += subset[Math.floor(Math.random() * subset.length)];
+    }
+    return ans;
+}
